ad1e4d4ac49013c2e5dd34f2c2e265f422ed63f7
GuilhermeOLemos <guilherme.oslemos@gmail.com>
2024-06-11 11:47:31 -0300

    Correção do Footer
    Co-authored-by: Júlio César Brito <julinh0brito@users.noreply.github.com>
    Co-authored-by: Ricardo A <ricardo-aj2@users.noreply.github.com>
    Co-authored-by: ju <juliacmps@users.noreply.github.com>
    Co-authored-by: LuanaRdS <LuanaRdS@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
 
@@ -14,22 +14,35 @@ def html2():
 def html3():
     return render_template('contato.html')
 
+@app.route('/acessoContato', methods = ['POST'])
+def acessoContato():
+    email = request.form.get('emailCliente')
+    nome = request.form.get('nomeCliente')
+    mensagem = request.form.get('mensagemCliente')
+
+    
+
+    return redirect('/contato.html') 
+
 @app.route('/menu_bar.html') ## utilizado para definir Rotas(URLS) e nesse caso a rota está fazendo a leitura de um arquivo e renderizando o no HTML
-def html6():
+def html4():
     return render_template('menu_bar.html')
 
 @app.route('/sobre.html') ## utilizado para definir Rotas(URLS) e nesse caso a rota está fazendo a leitura de um arquivo e renderizando o no HTML
-def html7():
+def html5():
     return render_template('sobre.html')
 
 @app.route('/eventos.html') ## utilizado para definir Rotas(URLS) e nesse caso a rota está fazendo a leitura de um arquivo e renderizando o no HTML
-def html8():
+def html6():
     return render_template('eventos.html')
 
 @app.route('/seja-voluntario.html')
-def html9():
+def html7():
     return render_template('seja-voluntario.html')
 
+@app.route('/footer.html') ## utilizado para definir Rotas(URLS) e nesse caso a rota está fazendo a leitura de um arquivo e renderizando o no HTML
+def html8():
+    return render_template('footer.html')
 
 ##inicia o método principal para rodar a aplicação.
 if __name__ == '__main__':
